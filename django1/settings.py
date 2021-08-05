f7d19c3801223550b60d7b784410d1b588ea134f
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2021-08-05 12:11:46 -0300

    Adicionando as views

--- django1/settings.py
+++ django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lu)#sn0b0*5!81@)ta@wr5d7w1bvyx*jz1@s*2o-gn)y+%iizw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
